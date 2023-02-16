e2f341d22c128041fade95bce8f5c6e3bdb4d7ef
Verena Ferraro <vferraro@c1r5s6.42lausanne.ch>
2023-02-16 08:50:26 +0100

    ex05

--- Module01/ex05/main.cpp
+++ Module01/ex05/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vferraro <vferraror@student.42lausanne.    +#+  +:+       +#+        */
+/*   By: vferraro <vferraro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 15:00:12 by vferraro          #+#    #+#             */
-/*   Updated: 2023/02/15 13:57:22 by vferraro         ###   ########.fr       */
+/*   Updated: 2023/02/16 08:48:40 by vferraro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,36 @@
 int	main(void) {
 	Harl	harl;
 	std::string	complaining;
-	std::cout << "Choose your level of complaining" << std::endl;
+	std::cout << "Choose your level of complaining between 0 and 4, yes, 4 means all the complaing levels, you can't say I didn't warn you !" << std::endl;
 	std::cin >> complaining;
-	while (complaining > "3") {
-		std::cout << "Choose a level between 0 and 3 or all" << std::endl;
+	while (complaining > "4") {
+		std::cout << "SERIOUSLY ??? I SAIID : Choose a level between 0 and 4 !" << std::endl;
 		std::cin >> complaining;
 	}
-	while (complaining >= "0" && complaining < "4") {
+	while ((complaining >= "0" && complaining <= "4")) {
 		if (complaining == "0") {
 			harl.complain("debug");
 			std::cin >> complaining;
 		}
-		if (complaining == "1") {
+		else if (complaining == "1") {
 			harl.complain("info");
 			std::cin >> complaining;
 		}
-		if (complaining == "2") {
+		else if (complaining == "2") {
 			harl.complain("warning");
 			std::cin >> complaining;
 		}
-		if (complaining == "3") {
+		else if (complaining == "3") {
 			harl.complain("error");
 			std::cin >> complaining;			
 		}
+		else if (complaining == "4") {
+			harl.complain("debug");
+			harl.complain("info");
+			harl.complain("warning");
+			harl.complain("error");
+			break;
+		}	
 	}
-	if (complaining == "all") {
-		harl.complain("debug");
-		harl.complain("info");
-		harl.complain("warning");
-		harl.complain("error");
-	}	
 		return 0;
 }
