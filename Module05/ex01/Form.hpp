a7a2c44fca8e912dfd938dcc29e09bbc7e97b6eb
Nit4r4 <87270463+Nit4r4@users.noreply.github.com>
2023-03-16 19:08:00 +0100

    Ex01

--- Module05/ex01/Form.hpp
+++ Module05/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: vferraro <vferraror@student.42lausanne.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 12:44:45 by vferraro          #+#    #+#             */
-/*   Updated: 2023/03/16 12:53:06 by vferraro         ###   ########.fr       */
+/*   Updated: 2023/03/16 19:07:43 by vferraro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,41 @@
 # include <iostream>
 # include <string>
 
+# include "Bureaucrat.hpp"
+
 class Form
 {
 private:
 	std::string	const _name;
 	bool	const _notSign;
-	int		const _SignGrade;
-	int		const _execGrade
+	int		const _signGrade;
+	int		const _execGrade;
 
-public:
-	Form(/* args */);
+public :
+	Form();
 	~Form();
+	Form(std::string name, int signGrade, int execGrade);
+	Form(Form const &copy);
+	Form &operator=(Form const &assign);
+
+	void		beSigned(Bureaucrat &copy);
+
+	std::string	getName() const;
+	bool		getSign() const;
+	int			getSignGrade() const;
+	int			getExecGrade() const;
+
+	class GradeTooHighException : public std::exception {
+			public :
+				virtual const char *what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception {
+			public :
+				virtual const char *what() const throw();
+		};
 };
 
-
+std::ostream	&operator<<(std::ostream &out, const Form &obj);
 
 #endif
