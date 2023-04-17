fab819dcbb72646d3945defdfcec7c1040f9414c
Verena Ferraro <vferraro@c1r5s6.42lausanne.ch>
2023-04-17 14:07:04 +0200

    M8

---
+++ Module08/ex01/Span.hpp
@@ -0,0 +1,27 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+# include <iostream>
+# include <string>
+# include <algorithm>
+
+#endif
+
+class Span {
+	private:
+		unsigned int	_N;
+	public:
+		Span();
+		Span(unsigned int N);
+		~Span();
+		Span(const Span &cpy);
+		Span &operator=(const Span &assign);
+
+		addNumber() {
+			if (N)
+				throw 
+		}
+};
+
+shortestSpan() {}
+longestSpan() {}
