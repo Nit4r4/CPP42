456eeea39ec39a4302f6b7183d3b15e06ec96634
Verena Ferraro <vferraro@c1r5s6.42lausanne.ch>
2023-04-20 14:34:42 +0200

    M08_okTest10000

--- Module08/ex01/main.cpp
+++ Module08/ex01/main.cpp
@@ -22,8 +22,8 @@ int intMain() {
 	sp.addNumber(9);
 	sp.addNumber(11);
 
-	std::cout << sp.shortestSpan() << std::endl; 
-	std::cout << sp.longestSpan() << std::endl;
+	std::cout << "shortest span : " << sp.shortestSpan() << std::endl; 
+	std::cout << "longest span : " << sp.longestSpan() << std::endl;
 
 	return 0;
 
@@ -53,8 +53,8 @@ int intMainTest() {
 	sp.addNumber(0);
 	sp.addNumber(111);
 
-	std::cout << sp.shortestSpan() << std::endl; 
-	std::cout << sp.longestSpan() << std::endl;
+	std::cout << "shortest span : " << sp.shortestSpan() << std::endl; 
+	std::cout << "longest span : " << sp.longestSpan() << std::endl;
 
 	return 0;
 
@@ -89,13 +89,13 @@ int	intMainErr() {
 		std::cerr << "Error : " << e.what() << std::endl;
 	}
 	try {
-		std::cout << sp.shortestSpan() << std::endl; 
+		std::cout << "shortest span : " << sp.shortestSpan() << std::endl; 
 	}
 	catch (Span::SpanException &e) {
 		std::cerr << "Error : " << e.what() << std::endl;
 	}
 	try {
-		std::cout << sp.longestSpan() << std::endl;
+		std::cout << "longest span : " << sp.longestSpan() << std::endl;
 	}
 	catch (Span::SpanException &e) {
 		std::cerr << "Error : " << e.what() << std::endl;
@@ -104,6 +104,43 @@ int	intMainErr() {
 
 }
 
+#define BigNum 10000
 int	intMainDixMille() {
-	
+	std::cout << "\nBIG NUMBERS TEST :" << std::endl;
+	Span sp = Span(BigNum);
+
+	std::srand(std::time(nullptr));
+	// std::srand(std::time(nullptr) + std::chrono::high_resolution_clock::now().time_since_epoch().count()); // pour lancer l execution plus rapidement et avoir des nombres differents
+	for (int i = 0; i < BigNum; i++) {
+		int random_number = std::rand() % BigNum;
+		sp.addNumber(random_number);
+		// std::cout << "\nprint numbers :" << std::endl;
+		// std::cout << random_number << std::endl;
+	}
+
+	// // Créez un vecteur de nombres uniques et aléatoires mais pas compatible avec C++98 (C++11)
+	// std::vector<int> random_numbers(BigNum);
+	// std::iota(random_numbers.begin(), random_numbers.end(), 0);
+	// std::shuffle(random_numbers.begin(), random_numbers.end(), std::mt19937{std::random_device{}()});
+
+	// // Ajouter des nombres à la classe Span à partir du vecteur de nombres aléatoires
+	// Span sp = Span(BigNum);
+	// for (int i = 0; i < BigNum; i++) {
+	// 	sp.addNumber(random_numbers[i]);
+	// }
+
+	try {
+		std::cout << "shortest span : " << sp.shortestSpan() << std::endl;
+	}
+	catch (Span::SpanException &e) {
+		std::cerr << "Error : " << e.what() << std::endl;
+	}
+	try {
+		std::cout << "longest span : " << sp.longestSpan() << std::endl;
+	}
+	catch (Span::SpanException &e) {
+		std::cerr << "Error : " << e.what() << std::endl;
+	}
+
+	return 0;
 }
