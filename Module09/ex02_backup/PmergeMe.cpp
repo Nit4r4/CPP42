69f40711170c81e44f2b535fb36ef8493839ddc6
Verena Ferraro <vferraro@c1r5s6.42lausanne.ch>
2023-05-12 19:36:17 +0200

    err check

---
+++ Module09/ex02_backup/PmergeMe.cpp
@@ -0,0 +1,134 @@
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() : _countArgNumber(0) {}
+
+PmergeMe::~PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe& copy) {
+	*this = copy;
+}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& assign) {
+	_countArgNumber = assign._countArgNumber;
+	_vect = assign._vect;
+	_pairs = assign._pairs;
+	_sortVect = assign._sortVect;
+	_list = assign._list;
+	return *this;
+}
+
+int	PmergeMe::getArgNumber() {
+	return _countArgNumber;
+}
+
+void	PmergeMe::setArgNumber(int argNum) {
+	_countArgNumber = argNum;
+}
+
+// int	PmergeMe::getArgNumber(const int number) const {
+// 	std::vector<int>::const_iterator it = _vect.find(number);
+// 	if (it == _vect.end()) {
+// 		throw PmergeMe::PMMException("Number not found in vector");
+// 	}
+// 	return it->second;
+// }
+
+void	PmergeMe::sortNumberAlgo() {
+
+	// swapPairsVector();
+	// upFirstOfPairsVector();
+		// Affichage du résultat
+//   Parcourir le vecteur et imprimer chaque paire
+	for (std::vector<std::pair<int, int> >::iterator it = _pairs.begin(); it != _pairs.end(); ++it) {
+		std::cout << "Vecteur _paires trier : " << it->first << it->second << std::endl;
+	}
+
+	// for (size_t i = 0; i < _sortVect.size(); i++) {
+	// 	std::cout << _sortVect[i] << " ";
+	// }
+
+	// 	// Tri du vecteur _sortVect
+	// std::sort(_sortVect.begin(), _sortVect.end());
+
+	// // Insérer chaque élément de _sortVect dans vec1
+	// for (size_t i = 0; i < _sortVect.size(); i++) {
+	// 	std::vector<int>::iterator it = std::lower_bound(_pairs.begin(), _pairs.end(), _sortVect[i]);
+	// 	_pairs.insert(it, _sortVect[i]);
+	// }
+
+	// // Affichage du résultat
+	// for (size_t i = 0; i < _pairs.size(); i++) {
+	// 	std::cout << _pairs[i] << " ";
+	// }
+	// std::cout << std::endl;
+}
+
+int		PmergeMe::timeToProcessRange() {
+
+   // Prendre le temps de début
+	clock_t start = clock();
+
+	// Exécuter l'algorithme de tri
+	// ...
+
+	// Prendre le temps de fin
+	clock_t end = clock();
+
+	// Calculer le temps écoulé en secondes
+	double elapsed_time = double(end - start) / CLOCKS_PER_SEC;
+
+	std::cout << "Temps écoulé: " << elapsed_time << " secondes" << std::endl;
+
+	return elapsed_time;
+}
+
+
+void	PmergeMe::swapPairsVector()
+{
+	int	rig;
+
+	for (std::vector< std::pair<int, int> >::iterator it=_pairs.begin(); it != _pairs.end(); it++)
+	{
+		if ((*it).first > (*it).second) //(*it).first it's the 1st number of the pair
+		{
+			rig = (*it).first;
+			(*it).first = (*it).second; //(*it).second it's the 2nd number of the pair
+			(*it).second = rig;
+		}
+	}
+	std::sort(_pairs.begin(), _pairs.end());
+	std::vector< std::pair<int, int> >::iterator it=_pairs.begin();// a enlever ?
+	if ((*it).first == JOKER)
+	{
+		_pairs.insert(_pairs.end(), _pairs.front());
+		_pairs.erase(_pairs.begin());
+	}
+}
+
+// void	jokerProcess() {
+// 	std::sort(_pairs.begin(), _pairs.end());
+// 	std::vector< std::pair<int, int> >::iterator it=_pairs.begin();
+// 	if ((*it).first == -1)
+// 	{
+// 		_pairs.insert(_pairs.end(), _pairs.front());
+// 		_pairs.erase(_pairs.begin());
+// 	}
+// }
+
+void	PmergeMe::upFirstOfPairsVector() {
+	for (std::vector< std::pair<int, int> >::iterator it=_pairs.begin(); it != _pairs.end(); it++)
+	{
+		if ((*it).first == JOKER)
+			continue;
+		_sortVect.push_back((*it).first);
+	}
+}
+
+void	PmergeMe::printResultVector() {
+	for (std::vector<int>::iterator it=_sortVect.begin(); it != _sortVect.end(); it++)
+	{
+		std::cout << (*it) << " ";
+	}
+	std::cout << std::endl;
+}
+
---
+++ Module09/ex02_backup/PmergeMe.hpp
@@ -0,0 +1,53 @@
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+# include <iostream>
+# include <string>
+# include <vector>
+# include <list>
+# include <ctime>
+# include <stdlib.h>
+
+#define ERR "\e[31mError : \e[0m"
+#define RES "\033[32mRESULT : \e[0m"
+#define JOKER -1
+
+class PmergeMe {
+	private:
+		unsigned long int	_countArgNumber;
+		std::vector<int> _vect;
+		std::vector<std::pair<int, int> > _pairs;
+		std::vector<int> _sortVect;
+		std::list<int> _list;
+
+	public:
+		PmergeMe();
+		~PmergeMe();
+		PmergeMe(const PmergeMe& copy);
+		PmergeMe& operator=(const PmergeMe& assign);
+
+		int		getArgNumber();
+		void	setArgNumber(int argNum);
+		//int		getArgNumber(const std::string& number) const;
+		void	swapPairsVector();
+		// void	jokerProcess();
+		void	upFirstOfPairsVector();
+		void	oddNumArg(); //impaires
+		void	sortNumberAlgo();
+		int		timeToProcessRange();
+		void	printResultVector();
+		
+		class PMMException : public std::exception {
+			public:
+				PMMException(const char* msg) : _msg(msg) {}
+				const char* what() const throw() { 
+					return _msg;
+				}
+
+			private:
+				const char* _msg;
+		};
+};
+
+
+#endif
